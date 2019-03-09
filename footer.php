a32f41e6314c32c9876d63e6b5580f5c49986f0a
Miho Watanabe <39816991+molumoko@users.noreply.github.com>
2019-03-09 21:20:49 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+    <footer>
+        <div id="contact">
+            <h3>お問い合わせ</h3>
+      <p>
+        お問い合わせはメールからお願いします!
+      </p>
+      <div class="btn-wrap">
+        <a href="#" class="btn btn-primary">メールでのお問い合わせ</a>
+      </div>
+    </div>
+    <div id="copyright">
+      Copyright &copy; 2018 CINEMA BLOG All Rights Reserved.
+    </div>
+  </footer>
+  <?php wp_footer(); ?>
+</body>
+</html>
